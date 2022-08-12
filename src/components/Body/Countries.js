1720201a9783ee7e3a33c9022daedd6e48b6cff3
Zee Mudia <mudiasam42@gmail.com>
2022-08-12 11:22:35 +0100

    added functionality that allows a user to see info about an option selected from the dropdown menu of countries

--- src/components/Body/Countries.js
+++ src/components/Body/Countries.js
@@ -1,8 +1,23 @@
+import './Countries.css';
+
 function Countries (props) {
     console.log(props)
     return (
         <div>
-            <h3>{props.country.name.official}</h3>
+            <h3>Official Name: {props.country.name.official}</h3>
+            <p>Capital City: {props.country.capital}</p>
+            <p>Region: {props.country.region}</p>
+            <p>Latititude & Longitude: {props.country.latlng}</p>
+            <p>Landlocked: { String(props.country.landlocked) }</p>
+            <p>Flag: <img src={props.country.flags.png} alt="flag" className="flag"/></p>
+            <p>Coat of Arms: <img src={props.country.coatOfArms.png} alt="coat of Arms" className="coat_of_arms"/></p>
+            {
+                Object.keys(props.country.currencies)
+                .map((currencyKey) => <p key={props.country.tld}>Currency: {props.country.currencies[currencyKey].name}</p>)
+              
+            }
+            <p>Population: {props.country.population}</p>
+           
         </div>
     ) 
 }
